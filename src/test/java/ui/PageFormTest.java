bf7b5a6aced9bcb9810036f444992035839a4273
Alex <udinkik@mail.ru>
2025-05-06 23:08:03 +0300

    ref case

--- src/test/java/ui/PageFormTest.java
+++ src/test/java/ui/PageFormTest.java
@@ -1,14 +1,21 @@
+package ui;
+
+import Pages.Modal;
 import Pages.PractiseFormPage;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class UiTest extends BaseTest {
+public class PageFormTest extends BaseTest {
 
     private static final String MONTH = "May";
     private static final String YEAR = "1995";
     private static final String DAY = "14";
+    private static final String NAME = "Alexey";
+    private static final String LAST_NAME = "Yudin";
+    private static final String MAIL = "a.yudin@yandex.ru";
+    private static final String PHONE = "89997651255";
+    private static final String ADDRESS = "Moscow";
 
     @ParameterizedTest(name = "Выбор хобби: {0}")
     @DisplayName("Проверка заполнения формы")
@@ -25,6 +32,11 @@ public class UiTest extends BaseTest {
                 .uploadFileForm()
                 .setAddress()
                 .setState();
+        new Modal()
+                .assertDataName(NAME, LAST_NAME)
+                .assertDataPhoneAndEmail(MAIL, PHONE)
+                .assertDbday(DAY, MONTH, YEAR)
+                .
     }
 
 }
